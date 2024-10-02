ac4e34bf54e41de15b3b566578f55f712eeb586c
namanshetty25 <namanvshetty@gmail.com>
2024-10-02 10:06:25 +0530

    Initial commit

---
+++ Simple_quantum_circuit.py
@@ -0,0 +1,25 @@
+import pennylane as qml
+from pennylane import numpy as np
+import matplotlib.pyplot as plt
+n=3
+dev=qml.device("default.qubit",wires=n)
+@qml.qnode(dev)
+def quantum_circuit():
+    qml.PauliX(wires=1)
+    qml.Hadamard(wires=1)   #o means 1st qubit and 1 means 2nd
+    qml.PauliX(wires=0)
+    qml.PauliX(wires=2)
+    return qml.state()
+state=quantum_circuit()
+print("Final state=",state)
+drawer=qml.draw(quantum_circuit)
+print(drawer())
+def plot_bar(state):
+    probs=np.abs(state)**2
+    plt.bar(range(len(probs)),probs)
+    plt.xticks(range(len(probs)),['000','001','010','011','100','101','110','111'])
+    plt.xlabel('Basic States')
+    plt.ylabel('Probability')
+    plt.title('Qubit State probabilities')
+    plt.show()
+plot_bar(state)
